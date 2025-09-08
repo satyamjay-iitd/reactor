6e16f627b430a75f64240c5e62fda8794bb0117c
Satyam Jay <satyamjay030@gmail.com>
2025-09-08 17:22:42 +0530

    merged with master

--- rpc_client/src/apis/default_api.rs
+++ rpc_client/src/apis/default_api.rs
@@ -153,8 +153,8 @@ pub async fn get_status(configuration: &configuration::Configuration, ) -> Resul
         let content = resp.text().await?;
         match content_type {
             ContentType::Json => serde_json::from_str(&content).map_err(Error::from),
-            ContentType::Text => return Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::StatusResponse`"))),
-            ContentType::Unsupported(unknown_type) => return Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::StatusResponse`")))),
+            ContentType::Text => Err(Error::from(serde_json::Error::custom("Received `text/plain` content type response that cannot be converted to `models::StatusResponse`"))),
+            ContentType::Unsupported(unknown_type) => Err(Error::from(serde_json::Error::custom(format!("Received `{unknown_type}` content type response that cannot be converted to `models::StatusResponse`")))),
         }
     } else {
         let content = resp.text().await?;
